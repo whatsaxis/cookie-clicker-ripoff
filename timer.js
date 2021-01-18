2f3af7a6816587cca8a3a64fa7fb57c4be3baa41
whatsaxis <59597828+whatsaxis@users.noreply.github.com>
2021-01-18 17:14:22 +0200

    Updated timer.js
    Updated timer.js

--- timer.js
+++ timer.js
@@ -1,8 +1,7 @@
 function updatePointsFromPps() {
     if (points_per_second !== 0) {
-        // points += points_per_second.toPrecision(2);
         console.log(points_per_second);
-        points = ((points * 10) + (points_per_second * 10)) / 100;  // Again, to fix floating point errors, we multiply by x10 to transfer to Integers
+        points += points_per_second;
     }
 
     updatePoints();
